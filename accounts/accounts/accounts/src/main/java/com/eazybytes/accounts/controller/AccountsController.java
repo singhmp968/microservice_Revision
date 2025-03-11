becdbd2063fe89bcb4727eccb02cff3f68f27636
xinghmp968 <rahulsinghmp968@gmail.com>
2025-03-11 00:23:10 +0000

    fix bug added update account component

--- accounts/accounts/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/accounts/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,5 +1,27 @@
 package com.eazybytes.accounts.controller;
 
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.service.impl.AccountsServiceImpll;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value = "/api",produces = {MediaType.APPLICATION_JSON_VALUE})
+@AllArgsConstructor
 public class AccountsController {
+AccountsServiceImpll accountsService;
+@PostMapping("/create")
+    public ResponseEntity<String> createAccount(@RequestBody CustomerDto customerDto) {
+    accountsService.createAccount(customerDto);
+    return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Account created successfully");
+    }
 }
--- accounts/accounts/accounts/src/main/java/com/eazybytes/accounts/repository/AccountsRepository.java
+++ accounts/accounts/accounts/src/main/java/com/eazybytes/accounts/repository/AccountsRepository.java
@@ -1,6 +1,7 @@
 package com.eazybytes.accounts.repository;
 
 import com.eazybytes.accounts.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
