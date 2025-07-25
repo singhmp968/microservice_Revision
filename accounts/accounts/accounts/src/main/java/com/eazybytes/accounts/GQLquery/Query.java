dc4abff828f2b66038a36628a62d2d877507ab62
xinghmp968 <rahulsinghmp968@gmail.com>
2025-07-26 00:44:14 +0100

    practice parametre in GraphQL

--- accounts/accounts/accounts/src/main/java/com/eazybytes/accounts/GQLquery/Query.java
+++ accounts/accounts/accounts/src/main/java/com/eazybytes/accounts/GQLquery/Query.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.GQLquery;
 
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -8,5 +9,10 @@ public class Query {
     @QueryMapping
     public String hello() {
         return "Hello GQL";
+    }
+    @QueryMapping
+    public String paramsRevise(@Argument String fName, @Argument String lName) {
+
+        return fName + " " + lName;
     }
 }
