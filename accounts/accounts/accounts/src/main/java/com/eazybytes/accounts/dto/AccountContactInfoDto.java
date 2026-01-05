f766e03e2d6e951a9bdc670e285066b527ad4392
singhmp968 <rahulsinghmp968@gmail.com>
2026-01-05 22:25:17 +0000

    completed git config

--- accounts/accounts/accounts/src/main/java/com/eazybytes/accounts/dto/AccountContactInfoDto.java
+++ accounts/accounts/accounts/src/main/java/com/eazybytes/accounts/dto/AccountContactInfoDto.java
@@ -4,7 +4,39 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
+//@ConfigurationProperties(prefix = "accounts")
+//public record AccountContactInfoDto(String message, Map<String,String> contactDetails, List<String> onCallSupport){
+//
+//}
+// below will allow to change property at run time
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDto(String message, Map<String,String> contactDetails, List<String> onCallSupport){
+public class AccountContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(Map<String, String> contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<String> getOnCallSupport() {
+        return onCallSupport;
+    }
+
+    public void setOnCallSupport(List<String> onCallSupport) {
+        this.onCallSupport = onCallSupport;
+    }
 
 }
